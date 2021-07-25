c5d0bc3a17ac7d69df73ce7f3e3148db772aa390
leejuhyeon <esloven3@gmail.com>
2021-07-25 18:13:26 +0900

    Create IOS project

---
+++ basic_lee/basic_lee/basic_leeApp.swift
@@ -0,0 +1,17 @@
+//
+//  basic_leeApp.swift
+//  basic_lee
+//
+//  Created by Juhyeon Lee on 2021/07/25.
+//
+
+import SwiftUI
+
+@main
+struct basic_leeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
